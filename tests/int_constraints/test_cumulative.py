25c45478a9a984705c70dfcb985f5a655d4d4f45
Dimitri <dimitri.justeau@gmail.com>
2022-09-23 15:51:24 +0200

    Fix cumulative test

--- tests/int_constraints/test_cumulative.py
+++ tests/int_constraints/test_cumulative.py
@@ -9,6 +9,6 @@ class TestCumulative(unittest.TestCase):
         m = Model()
         t1 = m.task(m.intvar(9), m.intvar(6), m.intvar(15))
         t2 = m.task(m.intvar(8), m.intvar(0, 6), m.intvar(8, 14))
-        m.cumulative([t1, t1], [m.intvar(1), m.intvar(1)], m.intvar(1)).post()
+        m.cumulative([t1, t2], [m.intvar(1), m.intvar(1)], m.intvar(1)).post()
         m.get_solver().solve()
         self.assertEqual(t2.duration.get_value(), 0)
