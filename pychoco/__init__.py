f0435c00aae7f90a87546b98f2330059b38b245b
Dimitri <dimitri.justeau@gmail.com>
2022-09-27 13:26:20 +0200

    Import essential functionalities in __init__

--- pychoco/__init__.py
+++ pychoco/__init__.py
@@ -19,3 +19,10 @@ def _module_cleanup_function():
 
 atexit.register(_module_cleanup_function)
 del atexit
+
+from .model import Model
+from .objects.graphs.undirected_graph import create_undirected_graph, create_complete_undirected_graph
+from .objects.graphs.directed_graph import create_directed_graph, create_complete_directed_graph
+from .objects.automaton.finite_automaton import FiniteAutomaton
+from .objects.automaton.cost_automaton import CostAutomaton
+from .objects.graphs.multivalued_decision_diagram import MultivaluedDecisionDiagram
