5966284a2255ad2ae0c321ee702d1c91a82f5a86
cprudhom <charles.prudhomme@imt-atlantique.fr>
2023-07-11 09:55:34 +0200

    Prepare docs for nbsphinx + requirements_docs.txt

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,12 @@ release = '0.1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'nbsphinx',
+    'sphinx_gallery.load_style',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
