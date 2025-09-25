a337f34439984c4c5f5fe87efb63bed20c048f13
Dimitri Justeau-Allaire <dimitri.justeau@gmail.com>
2025-09-25 12:02:15 +0200

    Update to Choco-solver 4.10.18

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
         'build_ext': CustomBuildExt,
         'build': CustomBuild,
     },
-    version='0.2.3',
+    version='0.2.4',
     author="Dimitri Justeau-Allaire, Charles Prud'homme",
     author_email="dimitri.justeau@gmail.com, charles.prudhomme@imt-atlantique.fr",
     description="Python bindings to the Choco Constraint Programming solver",
